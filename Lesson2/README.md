69005d14157e54f5e5bcc47278379d33fa4e6728
kawrou <121672581+kawrou@users.noreply.github.com>
2024-05-15 12:54:56 +0100

    Basics on defining types

--- Lesson2/README.md
+++ Lesson2/README.md
@@ -7,6 +7,10 @@ Install:
 Compiling:
 Typescript needs to be compiled into vanilla JS before it can be ran. Though there are packages you can install to let you run it without you having to manually compile each time. 
 - npx tsc index.ts
+or
+- tsc index.ts 
+
+if you've installed it globally or have created correct configurations. 
 
 The command compiles the file into "index.js" which you can run with node. 
 
--- Lesson2/index.ts
+++ Lesson2/index.ts
@@ -1,4 +1,45 @@
-let age = 30;
+//explicit type annotation. TS can also infer types. 
+let age: number = 30;
+let firstName: string = "Mario";
+let isFictional: boolean; 
 
-console.log(age);
+//Type errors example
 
+// age = 'hello'
+// Type error as 'age' is type of number
+age = 31
+
+// firstName = 100
+// Type error as 'firstName' is type of string
+firstName = 'Luigi'
+
+// isFictional = 'false'
+// Type error as 'isFictional' is type of boolean
+isFictional = true
+
+
+
+//auto type inference. If you hover over the variable, you can see type
+let planet = "Earth";
+let moons = 1;
+let isLarge = false;
+
+//You can see similar type error as above in following examples if you uncomment
+// planet = 1
+planet = 'Saturn'
+
+// moons = '145'
+moons = 145
+
+// isLarge = 'yes'
+isLarge = true; 
+
+
+//null & undefined
+let something: null
+let anotherThing: undefined
+
+// something = 10
+something = null
+// anotherThing = 'hello, world'
+anotherThing = undefined
